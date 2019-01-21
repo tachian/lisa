fa7749e5389a112d92b9e048dc3ced93a7aaffbd
Andre Tachian <andre.tachian.ext@nextel.com.br>
2019-01-21 11:01:48 -0200

    'wip'

--- lisa/models/__init__.py
+++ lisa/models/__init__.py
@@ -2,11 +2,13 @@ import os
 from mongoengine import *
 
 host = os.environ['MONGODB_URI'] if 'MONGODB_URI' in os.environ else 'http://localhost'
+db = os.environ['MONGODB_DB'] if 'MONGODB_DB' in os.environ else 'lisa'
 env = os.environ['ENV'] if 'ENV' in os.environ else 'development'
 
 if env == 'development':
-  connect(db='lisa')
+  connect(db=db)
 else:
   connect(
-    db='lisa',
-    host=host)
+    db=db,
+    host=host
+  )
